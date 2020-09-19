51ea855178751f0a0e923839f3c101fde5930ef1
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-19 09:45:25 +0000

    Installed django allauth and updated allauth in settings.py

--- deathfret_guitar/settings.py
+++ deathfret_guitar/settings.py
@@ -37,6 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    # Social accounts
+    """
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.instagram',
+    """
 ]
 
 MIDDLEWARE = [
@@ -66,6 +77,17 @@ TEMPLATES = [
         },
     },
 ]
+
+# Allows users and admins to login via email
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'deathfret_guitar.wsgi.application'
 
