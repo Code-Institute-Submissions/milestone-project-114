8a47767f2fafe9e12ea445cd634fe4a7fff73ee8
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-21 14:15:42 +0000

    Added artist, masterclass and lesson models. Updated admin view

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Artist, MasterclassOverview, Masterclass
 
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'artist_friendly_name',
+    )
 
-admin.site.register(Artist)
+    ordering = ('artist_friendly_name',)
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = (
+        'lesson_number',
+        'lesson_title',
+        'masterclass'
+    )
+
+admin.site.register(Artist, ArtistAdmin)
 admin.site.register(MasterclassOverview)
-admin.site.register(Masterclass)
+admin.site.register(Masterclass, LessonAdmin)
--- lessons/models.py
+++ lessons/models.py
@@ -3,28 +3,42 @@ from django.db import models
 
 class Artist(models.Model):
     artist_name = models.CharField(max_length=254)
+    artist_friendly_name = models.CharField(max_length=254, null=True, blank=True)
     discipline1 = models.CharField(max_length=254)
     discipline2 = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     skill_level1 = models.CharField(max_length=254)
     skill_level2 = models.CharField(max_length=254, null=True, blank=True)
     skill_level3 = models.CharField(max_length=254, null=True, blank=True)
+    artist_image = models.ImageField()
 
     def __str__(self):
         return self.artist_name
 
+    def get_artist_friendly_name(self):
+        return self.artist_friendly_name
+
 class MasterclassOverview(models.Model):
-    artist_name = models.ForeignKey('Artist', null=True, blank=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name_plural = 'Masterclasses'
+
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
     masterclass_title = models.CharField(max_length=254)
 
     def __str__(self):
-        return self.artist
+        return self.masterclass_title
 
 class Masterclass(models.Model):
-    masterclass_title = models.ForeignKey('MasterclassOverview', null=True, blank=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name_plural = 'Masterclass Lessons'
+
+    masterclass = models.ForeignKey('MasterclassOverview', on_delete=models.CASCADE)
     lesson_title = models.CharField(max_length=254)
+    lesson_number = models.IntegerField()
     lesson_description = models.TextField()
     video_url = models.URLField(max_length=1024)
 
     def __str__(self):
-        return self.masterclass_title
+        return self.lesson_title
