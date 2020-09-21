320d205202360c6f29e9a5d804befdcec64f53ba
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-21 17:28:55 +0000

    Added subscription page

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Subscription
 
-# Create your views here.
+
+def subscribe(request):
+    """ View to render the subscription page """
+
+    subscriptions = Subscription.objects.all()
+
+    context = {
+        'subscriptions': subscriptions,
+    }
+
+    return render(request, 'subscribe/subscribe.html', context)
