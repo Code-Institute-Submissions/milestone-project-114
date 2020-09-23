5e26703af65de7244a3f931105dc3d3fc43989a2
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-23 13:12:53 +0000

    Updated merch cart context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
+from django.shortcuts import get_object_or_404
 from decimal import Decimal
 from django.conf import settings
+from merch.models import Merch
 
 
 def cart_contents(request):
@@ -9,6 +11,17 @@ def cart_contents(request):
     item_count = 0
     delivery = total * Decimal(settings.DELIVERY_PERCENTAGE)
     grand_total = total + delivery
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        item = get_object_or_404(Merch, pk=item_id)
+        total += quantity * item.price
+        item_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'item': item,
+        })
 
     context = {
         'cart_items': cart_items,
