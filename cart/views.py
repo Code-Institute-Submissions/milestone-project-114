348d8dc6aeec64da3fc6c25035fff0815ebfbd6c
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-23 13:02:59 +0000

    Added functionality for user to add merch item to cart

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ View to render the subscription/merch cart """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add quantity of specified item to the merch cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
