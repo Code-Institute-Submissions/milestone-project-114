5012e55a5c9b90a92dfcb3f5151cf3d2e10874bb
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-24 10:10:31 +0000

    Added the merch item size logic to cart views.py. Updated context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,15 +11,27 @@ def cart_contents(request):
     item_count = 0
     cart = request.session.get('cart', {})
 
-    for item_id, quantity in cart.items():
-        item = get_object_or_404(Merch, pk=item_id)
-        total += quantity * item.price
-        item_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'item': item,
-        })
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            item = get_object_or_404(Merch, pk=item_id)
+            total += item_data * item.price
+            item_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'item': item,
+            })
+        else:
+            item = get_object_or_404(Merch, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * item.price
+                item_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'item': item,
+                    'size': size,
+                })
 
     delivery = total * Decimal(settings.DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
