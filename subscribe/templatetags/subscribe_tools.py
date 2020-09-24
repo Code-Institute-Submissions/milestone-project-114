0f64e74ae93113cfd82adf8f0f6e5f7b15a8c74e
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-24 17:46:54 +0000

    Added percentage saved logic to subscribe views.py

--- subscribe/templatetags/subscribe_tools.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-
-register = template.Library()
-
-
-@register.filter(name='calculate_percentage_saved')
-def calculate_percentage_saved():
-    return 
--- subscribe/views.py
+++ subscribe/views.py
@@ -6,9 +6,19 @@ def subscribe(request):
     """ View to render the subscription page """
 
     subscriptions = Subscription.objects.all()
+    monthly_prices = []
+
+    for price in subscriptions:
+        monthly_prices.append(int(price.monthly_price))
+
+    price1 = monthly_prices[0]
+    price2 = monthly_prices[1]
+
+    percentage_saved = ((price1 - price2)/((price1 + price2) / 2) * 100)
 
     context = {
         'subscriptions': subscriptions,
+        'percentage_saved': percentage_saved
     }
 
     return render(request, 'subscribe/subscribe.html', context)
