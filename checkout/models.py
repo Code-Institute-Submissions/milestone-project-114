6470fcf1d945b09f095bc8809d073114284ea4ce
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-25 14:20:29 +0000

    Added forms, admin and signals to the checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -27,7 +27,11 @@ class Order(models.Model):
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(
+            Sum(
+                'lineitem_total'
+            )
+        )['lineitem_total__sum']
         self.delivery_cost = self.order_total * settings.DELIVERY_PERCENTAGE / 100
         self.grand_total = self.order_total + self.delivery_cost
         self.save
