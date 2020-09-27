9a4b1d3fb770cd865c4ed93d29eb9a25e6ced24d
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-27 14:07:37 +0000

    Added stripe form submit functionality to ensure submission success

--- deathfret_guitar/settings.py
+++ deathfret_guitar/settings.py
@@ -170,3 +170,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DELIVERY_PERCENTAGE = 10
+
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
