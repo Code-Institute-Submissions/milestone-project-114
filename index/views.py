bbb563d5ca52a4ed2689320cd4dc89eff1b94f19
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-28 19:28:01 +0000

    Added all features.html and template pagninator for posts

--- index/views.py
+++ index/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Features
 
 
@@ -12,3 +13,20 @@ def index(request):
     }
 
     return render(request, 'index/index.html', context)
+
+
+def features(request):
+    """ View to render the home page """
+
+    features = Features.objects.all().order_by('id').reverse()
+    paginator = Paginator(features, 9)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'features': features,
+        'page_obj': page_obj
+    }
+
+    return render(request, 'index/features.html', context)
