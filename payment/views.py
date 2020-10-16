30919f1efa2997b1df0023fb137eef06398bc1b8
Shaun Lindsley <shaunlindsley@gmail.com>
2020-10-16 14:30:48 +0000

    Updated stripe payment confirm js for subscription payment

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import JsonResponse
 import stripe
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -21,6 +22,8 @@ def post(self, request, *args, **kwargs):
     data = request.POST
     customer_id = request.user.stripe_customer_id
 
+    print(customer_id)
+
     try:
         # Attach the payment method to the customer
         stripe.PaymentMethod.attach(
@@ -29,7 +32,7 @@ def post(self, request, *args, **kwargs):
         )
         # Set the default payment method on the customer
         stripe.Customer.modify(
-            data['customerId'],
+            customer_id,
             invoice_settings={
                 'default_payment_method': data['paymentMethodId'],
             },
@@ -37,15 +40,21 @@ def post(self, request, *args, **kwargs):
 
         # Create the subscription
         subscription = stripe.Subscription.create(
-            customer=data['customerId'],
+            customer=customer_id,
             items=[
                 {
-                    'price': 'price_HGd7M3DV3IMXkC'
+                    'price': data['priceId'],
                 }
             ],
             expand=['latest_invoice.payment_intent'],
         )
-        return jsonify(subscription)
+
+        data = {}
+        data.update(subscription)
+
+        return JsonResponse(data)
     except Exception as e:
-        return jsonify(error={'message': str(e)}), 200
+        return JsonResponse({
+            'error': {'message': str(e)}
+        })
 
