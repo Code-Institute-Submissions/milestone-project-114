9f5e3ae91ddd14ea308a2589045bc3b5d7c0a08c
Shaun Lindsley <shaunlindsley@gmail.com>
2020-10-22 17:44:01 +0000

    Added contexts.py test

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestCartContexts(TestCase):
+
+    def test_user_subscription_status(self):
+        status = 'paid'
+
+        if status == 'paid':
+            total = 10
+            delivery = total * (10 / 100)
+            discount = total * (10 / 100)
+            grand_total = delivery + total - discount
+            self.assertEqual(grand_total, 10)
+        else:
+            total = 10
+            delivery = total * (10 / 100)
+            grand_total = delivery + total
+            self.assertEqual(grand_total, 11)
--- checkout/test_checkout_models.py
+++ checkout/test_checkout_models.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
-from .models import Order, OrderLineItem
+from .models import Order
 
 
 class TestCheckoutModels(TestCase):
 
     def test_order_id_defaults_to_true(self):
-        order = Order.objects.create(order_number=1234)
+        order = Order.objects.create(order_number='1234')
         self.assertTrue(order.id)
+
+    def test_order_string_method_returns_order_number(self):
+        order = Order.objects.create(order_number='1234')
+        self.assertEqual(str(order), '1234')
